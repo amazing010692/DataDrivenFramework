086ab83386203f201257c6f125bd50f6c0c20946
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-03 23:04:28 +0800

    3rd commit - Data-driven - Creating Base Class

--- src/test/java/io/github/amazing010692/base/TestBase.java
+++ src/test/java/io/github/amazing010692/base/TestBase.java
@@ -1,11 +1,16 @@
 package io.github.amazing010692.base;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
 public class TestBase {
 	
+	@BeforeSuite
 	public void setUp() {
 		
 	}
 	
+	@AfterSuite
 	public void tearDown() {
 		
 	}
