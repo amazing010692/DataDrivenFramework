fb35be5ca0b8fb400dedc9240c623ccf6fa60ef2
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-04 20:24:12 +0800

    11th commit - Data-driven - Creating Base Class

--- src/test/java/io/github/amazing010692/rough/TestProperties.java
+++ src/test/java/io/github/amazing010692/rough/TestProperties.java
@@ -1,18 +1,22 @@
 package io.github.amazing010692.rough;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Properties;
 
 public class TestProperties {
 	
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		
 		System.out.println(System.getProperty("user.dir"));
 		Properties config = new Properties();
 		
 		//Path of Config.properties file.
 		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\Config.properties");
+		
+		//Load the configuration file.
+		config.load(fis);
 	}
 
+	
 }
