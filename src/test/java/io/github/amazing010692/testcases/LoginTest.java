a19da6c87422b0fcd5c16d69192fa05affcbed0d
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-07 23:35:52 +0800

    10th commit - Data-driven - Calling Properties with Test Case

--- src/test/java/io/github/amazing010692/testcases/LoginTest.java
+++ src/test/java/io/github/amazing010692/testcases/LoginTest.java
@@ -1,7 +1,12 @@
 package io.github.amazing010692.testcases;
 
-public class LoginTest {
+import org.testng.annotations.Test;
+
+import io.github.amazing010692.base.TestBase;
+
+public class LoginTest extends TestBase {
 	
+	@Test
 	public void loginAsBankManager() {
 		
 	}
