04450d697a2c593d3b5d2c1aff24a0792bc8504b
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-08 21:01:29 +0800

    4th commit - Data-driven - Initializing logs

--- src/test/java/io/github/amazing010692/base/TestBase.java
+++ src/test/java/io/github/amazing010692/base/TestBase.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -25,6 +26,7 @@ public class TestBase {
 	public static Properties config = new Properties();
 	public static Properties OR = new Properties();
 	public static FileInputStream fis;
+	public static Logger log = Logger.getLogger("devpinoyLogger");
 	
 	@BeforeSuite
 	public void setUp() {
@@ -37,6 +39,7 @@ public class TestBase {
 			}
 			try {
 				config.load(fis);
+				log.debug("Config file loaded !!!");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
