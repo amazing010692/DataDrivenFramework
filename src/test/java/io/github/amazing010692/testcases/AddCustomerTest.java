dbba67b44c50482d8eb38b72c531d67647b199dc
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-12 17:11:03 +0800

    13th commit - Data-driven - Adding Data Provider and Create another test

--- src/test/java/io/github/amazing010692/testcases/AddCustomerTest.java
+++ src/test/java/io/github/amazing010692/testcases/AddCustomerTest.java
@@ -1,11 +1,17 @@
 package io.github.amazing010692.testcases;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class AddCustomerTest {
 	
 	@Test(dataProvider = "getData")
 	public void addCustomer() {
+		
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
 		
 	}
 
