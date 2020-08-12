4eda721a63323551a664d57546719152c4d0044e
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-12 19:43:43 +0800

    26th commit - Data-driven - Adding Data Provider and Create another test

--- src/test/java/io/github/amazing010692/testcases/AddCustomerTest.java
+++ src/test/java/io/github/amazing010692/testcases/AddCustomerTest.java
@@ -1,6 +1,9 @@
 package io.github.amazing010692.testcases;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -9,12 +12,17 @@ import io.github.amazing010692.base.TestBase;
 public class AddCustomerTest extends TestBase {
 	
 	@Test(dataProvider = "getData")
-	public void addCustomer(String firstName, String lastName, String postCode) {
+	public void addCustomer(String firstName, String lastName, String postCode, String alertText) throws InterruptedException {
 		driver.findElement(By.cssSelector(OR.getProperty("addCustBtn"))).click();
 		driver.findElement(By.cssSelector(OR.getProperty("firstname"))).sendKeys(firstName);
 		driver.findElement(By.cssSelector(OR.getProperty("lastname"))).sendKeys(lastName);
 		driver.findElement(By.cssSelector(OR.getProperty("postcode"))).sendKeys(postCode);
 		driver.findElement(By.cssSelector(OR.getProperty("addBtn"))).click();
+		
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		Assert.assertTrue(alert.getText().contains(alertText));
+		alert.accept();
+		Thread.sleep(3000);
 		
 		logger.info("Successfully Added a Customer");
 	}
