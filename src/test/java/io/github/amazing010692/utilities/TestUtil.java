b5520f60fc798c88d0fd4a6947915b87b7fe6042
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-16 16:38:01 +0800

    5th commit - Data-driven - Screenshots and report customization-1

--- src/test/java/io/github/amazing010692/utilities/TestUtil.java
+++ src/test/java/io/github/amazing010692/utilities/TestUtil.java
@@ -1,5 +1,16 @@
 package io.github.amazing010692.utilities;
 
-public class TestUtil {
+import java.io.File;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import io.github.amazing010692.base.TestBase;
+
+public class TestUtil extends TestBase {
+	
+	public static void captureScreenshot() {
+		File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	}
 
 }
