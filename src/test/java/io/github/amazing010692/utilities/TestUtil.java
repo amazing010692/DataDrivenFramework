bdc45a46b833bf611ff114afe799bd354911c47b
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-16 16:48:12 +0800

    6th commit - Data-driven - Screenshots and report customization-1

--- src/test/java/io/github/amazing010692/utilities/TestUtil.java
+++ src/test/java/io/github/amazing010692/utilities/TestUtil.java
@@ -1,7 +1,9 @@
 package io.github.amazing010692.utilities;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
@@ -9,8 +11,10 @@ import io.github.amazing010692.base.TestBase;
 
 public class TestUtil extends TestBase {
 	
-	public static void captureScreenshot() {
+	public static String screenshotPath;
+	public static void captureScreenshot() throws IOException {
 		File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(srcFile, new File(System.getProperty("user.dir") + "\\target\\surefire-reports\\html\\error.jpg"));
 	}
 
 }
