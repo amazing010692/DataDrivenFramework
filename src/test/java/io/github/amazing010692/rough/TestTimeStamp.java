52eeb26164cd6b3b6f7b7d8caaf84282e01dabef
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-16 17:47:26 +0800

    3rd commit - Data-driven - Screenshots and report customization-2

--- src/test/java/io/github/amazing010692/rough/TestTimeStamp.java
+++ src/test/java/io/github/amazing010692/rough/TestTimeStamp.java
@@ -1,9 +1,15 @@
 package io.github.amazing010692.rough;
 
+import java.util.Date;
+
 public class TestTimeStamp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Date d = new Date();
+		String screenshotName = d.toString().replace(":", "_").replace(" ", "_") + ".jpg";
+		System.out.println(screenshotName);
+		System.out.println(d);
 
 	}
 
