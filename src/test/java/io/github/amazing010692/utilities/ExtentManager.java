2add670224f6b5a7a1f6f1b051a4c1275ea59f7e
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-21 14:07:53 +0800

    3rd commit - Data-driven - Generating Extent Report

--- src/test/java/io/github/amazing010692/utilities/ExtentManager.java
+++ src/test/java/io/github/amazing010692/utilities/ExtentManager.java
@@ -1,9 +1,28 @@
 package io.github.amazing010692.utilities;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentManager {
-	
+
 	private static ExtentReports extent;
+
+	public static ExtentReports createInstance(String fileName) {
+		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(fileName);
+
+		htmlReporter.config().setTheme(Theme.DARK);
+		htmlReporter.config().setDocumentTitle("JJSG'S Test Automation Reports");
+		htmlReporter.config().setEncoding("utf-8");
+		htmlReporter.config().setReportName(fileName);
+
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+		extent.setSystemInfo("Test Automation Engineer", "Janielle Joy Gregorio");
+		extent.setSystemInfo("Organization", "Jan's Group of Testers");
+		extent.setSystemInfo("Build no", "JJSG-amazing010692");
+
+		return extent;
+	}
 
 }
