58f57b4676f89ea9cd4e501a7b02f85412f19c5b
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-21 18:56:11 +0800

    13th commit - Data-driven - Generating Extent Report

--- src/test/java/io/github/amazing010692/listeners/CustomListeners.java
+++ src/test/java/io/github/amazing010692/listeners/CustomListeners.java
@@ -1,6 +1,7 @@
 package io.github.amazing010692.listeners;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Date;
 
 import org.testng.ISuite;
@@ -12,6 +13,8 @@ import org.testng.Reporter;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.Markup;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
@@ -30,8 +33,7 @@ public class CustomListeners extends TestBase implements ITestListener, ISuiteLi
 	public static ThreadLocal<ExtentTest> testReport = new ThreadLocal<ExtentTest>();
 
 	@Override
-	public void onTestStart(ITestResult result) {
-		
+	public void onTestStart(ITestResult result) {	
 		ExtentTest test = extent.createTest(result.getTestClass().getName()+"     @TestCase : "+result.getMethod().getMethodName());
         testReport.set(test);
 		
@@ -48,25 +50,32 @@ public class CustomListeners extends TestBase implements ITestListener, ISuiteLi
 
 	@Override
 	public void onTestFailure(ITestResult result) {
-		System.setProperty("org.uncommons.reportng.escape-output", "false");
+		String excepionMessage  =Arrays.toString(result.getThrowable().getStackTrace());
+		testReport.get().fail("<details>" + "<summary>" + "<b>" + "<font color=" + "red>" + "Exception Occured: Click to see"
+				+ "</font>" + "</b >" + "</summary>" +excepionMessage.replaceAll(",", "<br>")+"</details>"+" \n");
+		
 		try {
+
 			TestUtil.captureScreenshot();
+			testReport.get().fail("<b>" + "<font color=" + "red>" + "Screenshot of failure" + "</font>" + "</b>",
+					MediaEntityBuilder.createScreenCaptureFromPath(TestUtil.screenshotName)
+							.build());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
 		}
-		Reporter.log("Click to see Screenshot");
-		Reporter.log("<a target=\"blank\" href=" + TestUtil.screenshotName + ">Screenshot</a>");
-		Reporter.log("<br>");
-		Reporter.log("<br>");
-		Reporter.log("<a target=\"blank\" href=" + TestUtil.screenshotName + "><img src=" + 
-		TestUtil.screenshotName + " height=200 width=200></img></a>");
+		
+		String failureLogg="TEST CASE FAILED";
+		Markup m = MarkupHelper.createLabel(failureLogg, ExtentColor.RED);
+		testReport.get().log(Status.FAIL, m);
 		
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
-		// TODO Auto-generated method stub
+		String methodName=result.getMethod().getMethodName();
+		String logText="<b>"+"Test Case:- "+ methodName+ " Skipped"+"</b>";		
+		Markup m=MarkupHelper.createLabel(logText, ExtentColor.YELLOW);
+		testReport.get().skip(m);
 		
 	}
 
