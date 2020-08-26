87346c74ef63d84a4521a0ec21498f4d93aa9c41
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-26 14:43:44 +0800

    8th commit - Data-driven - Creating a common data provider

--- src/test/java/io/github/amazing010692/utilities/TestUtil.java
+++ src/test/java/io/github/amazing010692/utilities/TestUtil.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.annotations.DataProvider;
 
 import io.github.amazing010692.base.TestBase;
 
@@ -23,6 +24,22 @@ public class TestUtil extends TestBase {
 		
 		FileUtils.copyFile(srcFile, new File(System.getProperty("user.dir") + 
 				"\\target\\surefire-reports\\html\\" + screenshotName));
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
+		String sheetName = "OpenAccountTest";
+		int rows = excel.getRowCount(sheetName);
+		int cols = excel.getColumnCount(sheetName);
+		
+		Object[][] data = new Object[rows - 1][cols];
+		
+		for (int rowNum = 2; rowNum <= rows; rowNum++) {	//2
+			for (int colNum = 0; colNum < cols; colNum++) {
+				data[rowNum - 2][colNum] = excel.getCellData(sheetName, colNum, rowNum);	//2
+			}
+		}
+		return data;
 	}
 
 }
