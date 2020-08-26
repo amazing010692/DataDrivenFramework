3e570a415200d737b890620f253aadb2654dd4da
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-26 15:08:02 +0800

    14th commit - Data-driven - Creating a common data provider

--- src/test/java/io/github/amazing010692/testcases/OpenAccountTest.java
+++ src/test/java/io/github/amazing010692/testcases/OpenAccountTest.java
@@ -12,20 +12,5 @@ public class OpenAccountTest extends TestBase {
 		
 	}
 	
-	@DataProvider
-	public Object[][] getData() {
-		String sheetName = "OpenAccountTest";
-		int rows = excel.getRowCount(sheetName);
-		int cols = excel.getColumnCount(sheetName);
-		
-		Object[][] data = new Object[rows - 1][cols];
-		
-		for (int rowNum = 2; rowNum <= rows; rowNum++) {	//2
-			for (int colNum = 0; colNum < cols; colNum++) {
-				data[rowNum - 2][colNum] = excel.getCellData(sheetName, colNum, rowNum);	//2
-			}
-		}
-		return data;
-	}
 
 }
