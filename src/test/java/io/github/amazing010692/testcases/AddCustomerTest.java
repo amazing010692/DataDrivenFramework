b7c60f6ac5f9020a1d6565861e09e283516f9504
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-26 14:50:22 +0800

    11th commit - Data-driven - Creating a common data provider

--- src/test/java/io/github/amazing010692/testcases/AddCustomerTest.java
+++ src/test/java/io/github/amazing010692/testcases/AddCustomerTest.java
@@ -27,20 +27,5 @@ public class AddCustomerTest extends TestBase {
 		Assert.fail("Customer not added successfully");
 	}
 	
-	@DataProvider
-	public Object[][] getData() {
-		String sheetName = "AddCustomerTest";
-		int rows = excel.getRowCount(sheetName);
-		int cols = excel.getColumnCount(sheetName);
-		
-		Object[][] data = new Object[rows - 1][cols];
-		
-		for (int rowNum = 2; rowNum <= rows; rowNum++) {	//2
-			for (int colNum = 0; colNum < cols; colNum++) {
-				data[rowNum - 2][colNum] = excel.getCellData(sheetName, colNum, rowNum);	//2
-			}
-		}
-		return data;
-	}
 
 }
