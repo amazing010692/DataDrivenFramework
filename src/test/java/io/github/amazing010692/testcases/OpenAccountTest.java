18075a27533fb3ec236b2ddb8010d8a3f4d250f0
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-26 14:27:59 +0800

    2nd commit - Data-driven - Creating a common data provider

---
+++ src/test/java/io/github/amazing010692/testcases/OpenAccountTest.java
@@ -0,0 +1,46 @@
+package io.github.amazing010692.testcases;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.amazing010692.base.TestBase;
+
+public class OpenAccountTest extends TestBase {
+	
+	@Test(dataProvider = "getData")
+	public void addCustomer(String firstName, String lastName, String postCode, String alertText) throws InterruptedException {
+		click("addCustBtn_CSS");
+		type("firstname_CSS", firstName);
+		type("lastname_XPATH", lastName);
+		type("postcode_CSS", postCode);
+		click("addBtn_CSS");
+		
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		Assert.assertTrue(alert.getText().contains(alertText));
+		alert.accept();
+		
+		logger.info("Successfully Added a Customer");
+		
+		Assert.fail("Customer not added successfully");
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
+		String sheetName = "AddCustomerTest";
+		int rows = excel.getRowCount(sheetName);
+		int cols = excel.getColumnCount(sheetName);
+		
+		Object[][] data = new Object[rows - 1][cols];
+		
+		for (int rowNum = 2; rowNum <= rows; rowNum++) {	//2
+			for (int colNum = 0; colNum < cols; colNum++) {
+				data[rowNum - 2][colNum] = excel.getCellData(sheetName, colNum, rowNum);	//2
+			}
+		}
+		return data;
+	}
+
+}
