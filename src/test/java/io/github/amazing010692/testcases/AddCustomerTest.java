e8c9913ab415d930e0e01f76e4d5a79dd3dd5b82
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-26 14:52:04 +0800

    12th commit - Data-driven - Creating a common data provider

--- src/test/java/io/github/amazing010692/testcases/AddCustomerTest.java
+++ src/test/java/io/github/amazing010692/testcases/AddCustomerTest.java
@@ -3,14 +3,14 @@ package io.github.amazing010692.testcases;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.github.amazing010692.base.TestBase;
+import io.github.amazing010692.utilities.TestUtil;
 
 public class AddCustomerTest extends TestBase {
 	
-	@Test(dataProvider = "getData")
+	@Test(dataProviderClass = TestUtil.class)
 	public void addCustomerTest(String firstName, String lastName, String postCode, String alertText) throws InterruptedException {
 		click("addCustBtn_CSS");
 		type("firstname_CSS", firstName);
