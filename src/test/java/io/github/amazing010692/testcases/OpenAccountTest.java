943fd78e100569f30ce5e15624ec346019922d80
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-08-31 19:13:41 +0800

    2nd commit - Data-driven - Adding parameterization

--- src/test/java/io/github/amazing010692/testcases/OpenAccountTest.java
+++ src/test/java/io/github/amazing010692/testcases/OpenAccountTest.java
@@ -1,5 +1,7 @@
 package io.github.amazing010692.testcases;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 import io.github.amazing010692.base.TestBase;
@@ -7,8 +9,15 @@ import io.github.amazing010692.utilities.TestUtil;
 
 public class OpenAccountTest extends TestBase {
 	
-	@Test(dataProviderClass = TestUtil.class, dataProvider = "getData")
+	@Test(dataProviderClass = TestUtil.class, dataProvider = "dp")
 	public void openAccountTest(String customer, String currency) throws InterruptedException {
+		click("openaccount_CSS");
+		select("customer_CSS", data.get("customer"));
+		select("currency_CSS", data.get("currency"));
+		click("process_CSS");
+		Thread.sleep(2000);
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		alert.accept();
 		
 	}
 	
