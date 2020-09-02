b5dabdc4a8402023714775c732bad00a6e015745
amazing010692 <hello_janielle1992@yahoo.com.ph>
2020-09-03 00:33:30 +0800

    2nd commit - Data-driven - Run modes for test suite data

--- src/test/java/io/github/amazing010692/testcases/AddCustomerTest.java
+++ src/test/java/io/github/amazing010692/testcases/AddCustomerTest.java
@@ -1,32 +1,39 @@
 package io.github.amazing010692.testcases;
 
+import java.util.Hashtable;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import io.github.amazing010692.base.TestBase;
 import io.github.amazing010692.utilities.TestUtil;
 
 public class AddCustomerTest extends TestBase {
-	
+
 	@Test(dataProviderClass = TestUtil.class, dataProvider = "dp")
-	public void addCustomerTest(String firstName, String lastName, String postCode, String alertText) throws InterruptedException {
+	public void addCustomerTest(Hashtable<String, String> data) throws InterruptedException {
+
+		if (!data.get("runmode").equals("Y")) {
+
+			throw new SkipException("Skipping the test case as the Run mode for data set is NO");
+		}
+
 		click("addCustBtn_CSS");
-		type("firstname_CSS", firstName);
-		type("lastname_XPATH", lastName);
-		type("postcode_CSS", postCode);
-		click("addBtn_CSS");
+		type("firstname_CSS", data.get("firstname"));
+		type("lastname_XPATH", data.get("lastname"));
+		type("postcode_CSS", data.get("postcode"));
+		click("addbtn_CSS");
 		Thread.sleep(2000);
-		
 		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-		Assert.assertTrue(alert.getText().contains(alertText));
+
+		Assert.assertTrue(alert.getText().contains(data.get("alerttext")));
 		alert.accept();
-		
-		logger.info("Successfully Added a Customer");
-		
+
 		Thread.sleep(2000);
+
 	}
-	
 
 }
--- src/test/java/io/github/amazing010692/testcases/OpenAccountTest.java
+++ src/test/java/io/github/amazing010692/testcases/OpenAccountTest.java
@@ -1,25 +1,33 @@
 package io.github.amazing010692.testcases;
 
+import java.util.Hashtable;
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import io.github.amazing010692.base.TestBase;
 import io.github.amazing010692.utilities.TestUtil;
 
 public class OpenAccountTest extends TestBase {
-	
+
 	@Test(dataProviderClass = TestUtil.class, dataProvider = "dp")
-	public void openAccountTest(String customer, String currency) throws InterruptedException {
+	public void openAccountTest(Hashtable<String, String> data) throws InterruptedException {
+
+		if (!(TestUtil.isTestRunnable("openAccountTest", excel))) {
+
+			throw new SkipException("Skipping the test " + "openAccountTest".toUpperCase() + "as the Run mode is NO");
+		}
+
 		click("openaccount_CSS");
-		select("customer_CSS", customer);
-		select("currency_CSS", currency);
+		select("customer_CSS", data.get("customer"));
+		select("currency_CSS", data.get("currency"));
 		click("process_CSS");
 		Thread.sleep(2000);
-		
 		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
 		alert.accept();
-		logger.info("Successfully Opened an Account");
+
 	}
-	
+
 }
